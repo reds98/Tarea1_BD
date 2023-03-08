23e93e7cd974b611d758fef147b7529ddd5c28ac
DiegoMu;oz <93402041+HackerInjectionSQL@users.noreply.github.com>
2023-03-07 20:32:47 -0600

    main edit

--- source/main.py
+++ source/main.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+import pymssql
+
+#     print("ID=%d, Name=%s" % (row['id'], row['name']))
+def obtenerArticulos():
+    conn = pymssql.connect("basescurso.database.windows.net", "adminBases", "Tantan20", "dbTarea1")
+    cursor = conn.cursor(as_dict=True)
+    results=[]
+    cursor.execute('EXEC dbo.SelectArticles;')
+    for row in cursor:
+        results.append(row)
+        print(row)
+    conn.close()
+    return results
 @app.route('/')
 def index():
      return render_template('index.html')
