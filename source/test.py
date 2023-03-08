ca12b7560687ca40978b5624c01d5f93f4c7b6e8
reds98 <sahid.rojas64@gmail.com>
2023-03-07 20:29:43 -0600

    Add Added the main template for getting the info

--- source/test.py
+++ source/test.py
@@ -1,11 +1,14 @@
 
 import pymssql
-conn = pymssql.connect("basescurso.database.windows.net", "adminBases", "Tantan20", "dbTarea1")
-cursor = conn.cursor(as_dict=True)
 
-cursor.execute('EXEC dbo.SelectArticles;')
-for row in cursor:
-    print(row)
 #     print("ID=%d, Name=%s" % (row['id'], row['name']))
-
-conn.close()
+def obtenerArticulos():
+    conn = pymssql.connect("basescurso.database.windows.net", "adminBases", "Tantan20", "dbTarea1")
+    cursor = conn.cursor(as_dict=True)
+    results=[]
+    cursor.execute('EXEC dbo.SelectArticles;')
+    for row in cursor:
+        results.append(row)
+        print(row)
+    conn.close()
+    return results
