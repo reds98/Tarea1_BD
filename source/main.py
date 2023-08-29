7f1d02119c0457eae578839e4e1bc4a8e5a41ee6
reds98 <sahid.rojas64@gmail.com>
2023-08-29 13:29:14 -0600

    Added the new DATABASE logic and correct Functionality working

--- source/main.py
+++ source/main.py
@@ -5,10 +5,10 @@ import pymssql
 
 #     print("ID=%d, Name=%s" % (row['id'], row['name']))
 def obtenerArticulos():
-    conn = pymssql.connect("basescurso.database.windows.net", "adminBases", "Tantan20", "dbTarea1")
+    conn = pymssql.connect("34.30.51.145", "sqlserver", "Tantan20", "Tarea1")
     cursor = conn.cursor(as_dict=True)
     results=[]
-    cursor.execute('EXEC dbo.SelectArticles;')
+    cursor.execute('EXEC dbo.GetArticulosOrdenados;')
     for row in cursor:
         results.append(row)
         print(row)
@@ -16,18 +16,48 @@ def obtenerArticulos():
     conn.close()
     return results
 #     print("ID=%d, Name=%s" % (row['id'], row['name']))
-def insertarArticulo(nombre,precio):
+# def insertarArticulo(nombre,precio):
     
-    conn = pymssql.connect("basescurso.database.windows.net", "adminBases", "Tantan20", "dbTarea1")
-    cursor = conn.cursor(as_dict=True)
-    results=[]
-    cursor.execute(f"EXEC InsertArticle @article_name ='{nombre}' , @price ={precio} ;")
-    for row in cursor:
-        results.append(row)
-        print(row)
-    conn.commit()
-    conn.close()
-    return results
+#     conn = pymssql.connect("basescurso.database.windows.net", "adminBases", "Tantan20", "dbTarea1")
+#     cursor = conn.cursor(as_dict=True)
+#     results=[]
+#     cursor.execute(f"EXEC dbo.InsertarArticulo @Nombre  ='{nombre}' , @Precio={precio} ;")
+#     for row in cursor:
+#         results.append(row)
+#         print(row)
+#     conn.commit()
+#     conn.close()
+#     return results
+def insertarArticulo(nombre, precio):
+    try:
+        # Establecer la conexión con SQL Server
+        conn = pymssql.connect("34.30.51.145", "sqlserver", "Tantan20", "Tarea1")
+        cursor = conn.cursor()
+
+        # Ejecutar el procedimiento almacenado para insertar un nuevo artículo
+        cursor.execute("EXEC dbo.InsertarArticulo @Nombre=%s, @Precio=%s", (nombre, precio))
+
+        # Obtener el resultado
+        result = cursor.fetchone()
+        codigo = result['Codigo']
+        mensaje = result['Mensaje']
+
+        # Confirmar la transacción
+        conn.commit()
+
+        # Imprimir el código y el mensaje
+        print(f"Código: {codigo}, Mensaje: {mensaje}")
+
+    except pymssql.Error as e:
+        # Manejar cualquier error que ocurra
+        print(f"Error: {e}")
+
+    finally:
+        # Cerrar la conexión
+        conn.close()
+        return result
+
+
 @app.route('/')
 def index():
      articulos=obtenerArticulos()
@@ -47,14 +77,14 @@ def createArticle():
             articlePrice=request.form["articlePrice"]
             print("DATA TO BE INSERTED ",articleName,articlePrice)
             resultInsert=insertarArticulo(articleName,articlePrice)
-            result=resultInsert[0]["Result"]
-            print("INSERT RESULT==>",resultInsert[0])
-            print("KEYS",result)
-            if(result=="Article with the name Martillo already exists."):
-                return render_template('create.html',error=result)
+            print("RESULT INSERTED==>",resultInsert)
+            print("Code INSERTED==>",resultInsert[0])
+            print("Message INSERTED==>",resultInsert[1])
+            if(resultInsert[0]==409):
+                return render_template('create.html',error=resultInsert[1])
             else:
                 articulos=obtenerArticulos()
-                return render_template('index.html',articulos=articulos,message=result) 
+                return render_template('index.html',articulos=articulos,message=resultInsert[1]) 
 
 
         
